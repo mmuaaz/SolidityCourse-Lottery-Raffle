9a3b70fb98d7a67186c95e525b1463f88067cc96
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-08-31 14:12:34 +0300

    add an enclosing } in deployer object.

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,6 +29,7 @@ module.exports = {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
             //Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
             player: {
                 default: 1,
             },
